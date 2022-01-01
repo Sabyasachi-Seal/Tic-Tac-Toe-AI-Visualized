d64485553112fdb78562ab325109d132619a425f
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-01-01 12:05:35 +0530

    inputs and play function

--- game.py
+++ game.py
@@ -14,3 +14,29 @@ class TicTacToe:
         number_board = [[str(i) for i in range(j*3, (j+1)*3)]]
         for row in number_board:
             print("|| " + " || ".join(row)+" ||")
+
+        
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot == " "]
+
+    def empty_squares(self):
+        return " " in self.board
+
+    def num_empty_squares(self):
+        return self.board.count(" ")
+
+    def make_move(self, square, letter):
+        if self.board[square] == " ":
+            self.board[square] = letter
+            return True
+        return False
+
+def play(self, x_player, o_player, print_game=True):
+    if print_game:
+        game.print_board_nums()
+    letter = "X"
+    while game.empty_squares():
+        if letter=="O":
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import math # for mathematical uses
 import random # for randomly choosing a value
+import game 
 
 class Player:
     def __init__(self, letter):
@@ -17,7 +18,8 @@ class RandomComputerPlayer(Player):
     
     
     def get_move(self, game):
-        pass
+        square = random.choice(game.available_moves())
+        return square
 
 class HumanPlayer(Player):
     def __init__(self, letter):
@@ -25,4 +27,15 @@ class HumanPlayer(Player):
     
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + "\'s Turn. Input (0-8): ")
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError()
+                valid_square = True
+            except ValueError():
+                print("Invalid Move. Try Again.")
+        return val
